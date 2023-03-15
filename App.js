5ff1c25149a435a43af88597092826e156f7f4e1
Amorim-EA <erick.amorim@estudante.ifms.edu.br>
2023-03-15 13:54:24 -0400

    Commit inicial

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+
+const getNomeCompleto = (p, s, t) => {return `${p} ${s} ${t}`};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>getNomeCompleto("Erick", "da Silva", "Amorim")</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
