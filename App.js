1253028aeaecec3094def45bace7e28b9487b7e0
Amorim-EA <erick.amorim@estudante.ifms.edu.br>
2023-03-22 14:31:52 -0400

    Adicionei codigo

--- App.js
+++ App.js
@@ -1,30 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react-native';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Text, View, Image, Button } from 'react-native';
+import { useState } from 'react';
 
-const Nome = () => {
+const Clickavel = (props) => {
+  const [isClicked, setIsClicked] = useState(true);
   return (
-  <View style={}>  
-  <Text>{}</Text>;
-  </View>
-  );
+    <View>
+      <Text>Esse é {props.nome} -> {{isClicked} ? "Clique-me" : "Clickado"}</Text>
+      <Button
+        onPress={() => {
+        setIsClicked(false)
+         }}
+         disabled={!isClicked}
+         title={isClicked ? "Clique-me" : "Thank you"}
+    />
+    </View>
+  )
 }
 
-const Tel = (props) => {
-  return (
-  <View style={}>  
-  <Text>{props}</Text>;
+const DetalharProduto = (props) => {
+  return(
+  <View style={{borderWidth: 1, margin: 50}}>
+    <Text>Poduto: {props.produto}</Text>
+    <Text>Marca: {props.marca}</Text>
+    <Text>Preço: {props.preco}</Text>
+    <Text>Quantidade em estoque: {props.q_estoque}</Text>
   </View>
   );
 }
 
 export default function App() {
   return (
-    <View style={}>
-     <Nome />
-     <Tel />
-     <TextInput 
-     defaultValue='Me de um nome pedin !'/>
-    </View>
+   <View>
+    <Image 
+    source={{uri: "https://www.shutterstock.com/image-photo/view-businessman-holding-computer-devices-260nw-1014468334.jpg"}} 
+    style={{width: 200, height: 200}} 
+    />
+    <DetalharProduto nome="Mochila" marca="Dell" preco="R$ 99,99" q_estoque="30"/>
+    <DetalharProduto nome="Mose" marca="Razer" preco="R$ 149,99" q_estoque="25"/>
+    <DetalharProduto nome="Memória Ram" marca="Kingston" preco="R$ 199,99" q_estoque="18"/>
+    <Clickavel nome="Botão 1" />
+    <Clickavel nome="Botão 2" />
+   </View>
   );
 }
